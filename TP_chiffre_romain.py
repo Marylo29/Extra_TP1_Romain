5a9e3ee7ef99934fbd36ad027e9be8c1265e4d76
Marylo29 <collin-n@saint-louis29.net>
2024-10-18 13:28:46 +0200

    Update TP_chiffre_romain.py

--- TP_chiffre_romain.py
+++ TP_chiffre_romain.py
@@ -43,7 +43,32 @@ def romain(valeur):
             valeur -= 1
     return txt
         
+"""
+Solution : 
+VALEURS = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+]
 
+def romain(valeur):
+    resultat = ""
+    for entier, symbole in VALEURS:
+        while valeur >= entier:
+            valeur -= entier
+            resultat += symbole
+    return resultat
+"""
 
 # Tests
 assert romain(4) == "IV"
